1aabae0e8ac0f67b20a991480075f290a2031873
yuwq1098 <1098654043@qq.com>
2017-03-18 20:38:29 +0800

    modify my project, adjust framework

--- js/my-app.js
+++ js/my-app.js
@@ -4,9 +4,36 @@ var myApp = new Framework7();
 // 出口选择器引擎定义
 var $$ = Dom7;
 
+// Init滑块和它的实例存储在mySwiper变量
+var mySwiper = myApp.swiper('.swiper-container', {
+
+    spaceBetween: 30,                       /*间距*/
+    pagination : '.swiper-pagination',       /*获取轮播容器*/
+    paginationType : 'bullets',              /*点分页显示*/
+    loop: true,                              /*开启循环*/
+    loopAdditionalSlides : 1,                /*从第几张图开始显示*/
+    autoplay: 2500,                          /*每次轮播间隔3秒时间*/
+    autoplayDisableOnInteraction : false,    /*用户操作后,继续开启autoplay,默认true为关闭*/
+    preloadImages: true,                     /*是否强制加载所有图片*/
+    width : window.innerWidth,               /*你的slide宽度,全屏写法*/
+    speed : 350,                             /*动画效果时间*/
+});
+
 // 添加视图
 var mainView = myApp.addView('.view-main', {
     // 因为我们使用固定通过导航条我们可以启用动态导航
+    dynamicNavbar: true
+});
+
+//给app首页启动动态导航
+var homeView = myApp.addView('#home', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
+    dynamicNavbar: true
+});
+
+//给个人中心启动动态导航
+var homeView = myApp.addView('#personal', {
+    // Because we use fixed-through navbar we can enable dynamic navbar
     dynamicNavbar: true
 });
 
